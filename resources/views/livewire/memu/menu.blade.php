a1edcaedac33493614c43fd0857a8666e5edef91
André Silva Paulino <andre.paulino@adtsys.com.br>
2023-06-07 17:42:59 -0300

    Finalizando partes da página inicial inicial

--- resources/views/livewire/memu/menu.blade.php
+++ resources/views/livewire/memu/menu.blade.php
@@ -1,21 +1,21 @@
-<header class="bg-yellow-500">
+<header class="bg-blue-800">
     <div class="container mx-auto flex items-center justify-between px-6 py-4">
       <div>
         <a href="#" class="text-white font-bold text-xl">Logo</a>
       </div>
       <nav class="hidden md:flex md:items-center md:space-x-4">
-        <a href="#" class="text-white hover:text-gray-200">Home</a>
-        <a href="#" class="text-white hover:text-gray-200">Sobre Nós</a>
-        <a href="#" class="text-white hover:text-gray-200">Parceiros</a>
-        <a href="#" class="text-white hover:text-gray-200">Contato</a>
+        <a href="#sobrenos" class="text-white hover:text-gray-200">Home</a>
+        <a href="#sistema" class="text-white hover:text-gray-200">Sistema</a>
+        <a href="#dica" class="text-white hover:text-gray-200">Dicas Semanais</a>
+        <a href="#contato" class="text-white hover:text-gray-200">Contato</a>
       </nav>
       <div class="hidden md:flex md:items-center md:space-x-4">
-        <button class="px-4 py-2 bg-red-500 text-white font-bold rounded-lg">Logar</button>
-        <button class="px-4 py-2 bg-red-500 text-white font-bold rounded-lg">Registrar</button>
+        <button class="px-4 py-2 bg-red-700 text-white font-bold rounded-lg">Logar</button>
+        <button class="px-4 py-2 bg-red-700 text-white font-bold rounded-lg">Registrar</button>
       </div>
       <div class="md:hidden">
         <button class="mobile-menu-button">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 text-white">
             <path d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
         </button>
@@ -25,13 +25,13 @@
     <div class="hidden mobile-menu">
       <nav class="px-4 py-2">
         <a href="#sobrenos" class="block text-white font-bold py-2">Home</a>
-        <a href="#" class="block text-white font-bold py-2">Sobre Nós</a>
-        <a href="#" class="block text-white font-bold py-2">Parceiros</a>
-        <a href="#" class="block text-white font-bold py-2">Contato</a>
+        <a href="#sistema" class="block text-white font-bold py-2">Sistema</a>
+        <a href="#dica" class="block text-white font-bold py-2">Dicas Semanais</a>
+        <a href="#contato" class="block text-white font-bold py-2">Contato</a>
       </nav>
       <div class="px-4 py-2">
-        <button class="block w-full bg-red-500 text-white font-bold rounded-lg py-2">Logar</button>
-        <button class="block w-full bg-red-500 text-white font-bold rounded-lg py-2 mt-2">Registrar</button>
+        <button class="block w-full bg-red-700 text-white font-bold rounded-lg py-2">Logar</button>
+        <button class="block w-full bg-red-700 text-white font-bold rounded-lg py-2 mt-2">Registrar</button>
       </div>
     </div>
   </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,16 +24,92 @@
     </div>
   </section>
 
-  <section class="bg-gray-100 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4 mt-5">
+  <section id="sistema" class="bg-gray-100 mt-5">
+    <div class="container mx-auto px-4 py-8 md:py-16">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
 
-    <div class="card mt-2 items-center justify-center">
-        <div><h3>Gramatica</h3></div>
-            <img src="{{ asset('images/flags.png') }}" alt="">
+        <div>
+          <h2 class="text-2xl font-bold mb-4">Sistema</h2>
+          <p class="text-rigth text-justify mb-3">
+            O Sistema foi desenvolvido para te axuliar na organização dos seus estudos de Inglês ou qualquer outra lingua,
+            falamos muito do inglês por que foi a motivação da criação do mesmo, mas pode ser aplicado a qualquer tipo de aprendizagem.  </p>
 
+            <p class="text-justify">A ideia central é que você cadastre seus estudos e enviaremos lembrete do que você precisa voltar a estudar na plataforma,
+                porque hoje o problema de muitos não é estudar mas sim lembrar que tem que estudar, temos algumas ferramentas no mercardo com o Anki que é um
+                aplicativi de memorização espaçada, mas se você não entra no aplicativo ele não vai te encaminhar nenhum e-mail te avisando, por isso estamos aquii,
+                para sanar esta lacuna.
+            </p>
+        </div>
+        <div>
+            <img src="{{ asset('images/multilingua.jpg') }}" alt="Imagem" class="w-full rounded-lg">
+        </div>
+      </div>
     </div>
-
   </section>
 
+  <section id="dica" class="py-8 bg-gray-100 mt-5">
+    <div class="container mx-auto">
+      <h2 class="text-2xl font-bold text-center mb-4">Dicas Semanais</h2>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div>
+          <div class="video-container">
+            <h3>Speaking</h3>
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/DZeBlZcN2s0" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div>
+          <div class="video-container">
+            <h3>Listening</h3>
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/vuTY4FDAbpA" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div>
+          <div class="video-container">
+            <h3>Speaking</h3>
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/5jGgcELhykc" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div>
+          <div class="video-container">
+            <h3>Reading</h3>
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/BlCBnY3Y-jw" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
+  <<section id="contato" class="py-8 bg-blue-800">
+    <div class="container mx-auto">
+      <h2 class="text-2xl font-bold text-center mb-4 text-white">Fale Conosco</h2>
+      <div class="max-w-lg mx-auto">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2 ml-1" for="name">
+              Nome:
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Seu nome">
+          </div>
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2 ml-1" for="email">
+              Email:
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Seu email">
+          </div>
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2 ml-1" for="message">
+              Mensagem:
+            </label>
+            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" placeholder="Digite sua mensagem"></textarea>
+          </div>
+          <div class="flex items-center justify-between">
+            <button class="bg-red-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-1 text-sm" type="button">
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 
 @endsection
