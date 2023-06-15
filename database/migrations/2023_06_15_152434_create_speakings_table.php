a92926cb482793d32827647981074365fa7cd04e
André Silva Paulino <andre.paulino@adtsys.com.br>
2023-06-15 13:00:08 -0300

    Criado as models e migration da aplicação

---
+++ database/migrations/2023_06_15_152434_create_speakings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('speakings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo',256);
+            $table->string('url');
+            $table->string('description',256);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('speakings');
+    }
+};
